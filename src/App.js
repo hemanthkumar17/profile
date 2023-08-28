f278b422ee4936e17537c600cc1d2137bedc963e
hemanthkumar17 <hemanth17112000@gmail.com>
2023-08-27 20:52:04 -0500

    Initializing webpage

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to My Profile</h1>
+        <img
+          src="static\images\profile_image.jpeg" // Replace with your profile image URL
+          alt="Profile"
+          style={{ width: '200px', borderRadius: '50%' }}
+        />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi! I'm Hemanth Kumar, a Student at Rice University. I love Deep Learning and Tinkering with technologies.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <a
+            href="https://github.com/hemanthkumar17"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            My GitHub Profile
+          </a>
+        </div>
       </header>
     </div>
   );
